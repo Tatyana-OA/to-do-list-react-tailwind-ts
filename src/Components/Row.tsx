cd3ca7949517d84f67bba298c221fd37b1adfa2f
Tatyana Asenova <tatyana.asenova@mentormate.com>
2022-02-17 13:12:04 +0200

    Row component created.

---
+++ src/Components/Row.tsx
@@ -0,0 +1,22 @@
+type Todo = {
+    id: string,
+    task: string,
+    isCompleted: boolean
+}
+
+type TodoProps = {
+    todo: Todo
+}
+
+
+const Row = ({todo: {task, isCompleted}}: TodoProps) => {
+  return (
+    <div>
+        <p>{task}</p>
+        <button aria-label="Delete a todo" onClick={()=>null}>X</button>
+        <input type="checkbox" checked={isCompleted} onChange={() => null} />
+    </div>
+  )
+}
+
+export default Row
