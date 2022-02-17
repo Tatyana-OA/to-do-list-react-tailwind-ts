c7f74fee4c8e65fa0e4cd9ab3f950647e55b69b9
Tatyana Asenova <tatyana.asenova@mentormate.com>
2022-02-17 15:48:34 +0200

    Check todo -> change the isCompleted status back and forth

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -7,15 +7,16 @@ type Todo = {
 type TodoProps = {
     todo: Todo,
     handleDeleteTodo: (id:string) => void // the prop takes in an id which is a string and returns void, a.k.a nothing
+    handleCheckTodo: (id:string) => void 
 }
 
 
-export const Row = ({todo: {task, isCompleted, id}, handleDeleteTodo}: TodoProps) => {
+export const Row = ({todo: {task, isCompleted, id}, handleDeleteTodo, handleCheckTodo}: TodoProps) => {
   return (
     <div>
         <p>{task}</p>
         <button aria-label="Delete a todo" onClick={() =>handleDeleteTodo(id)}>X</button>
-        <input type="checkbox" checked={isCompleted} onChange={() => null} />
+        <input type="checkbox" checked={isCompleted} onChange={() => handleCheckTodo(id)} />
     </div>
   )
 }
--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -16,13 +16,31 @@ export const Todos = () => {
     // data is expected to be of type -> array of Todos
     const [todos, setTodos] = useState<Todo[]>(data)
 
+
+    //Deletion
 const handleDeleteTodo = (id:string) => {
     const updatedTodos = todos.filter((todo) => todo.id !=id);
     setTodos(updatedTodos)
 }
+
+    //Check off the list by changing the current value of todos
+const handleCheckTodo = (id:string) => {
+    const updatedTodos = todos.map((todo) => {
+        if (todo.id===id) {
+            return {
+                ...todo,
+                isCompleted: !todo.isCompleted,
+        }
+        }
+
+        // return the todo if the ids don't match
+        return todo;
+    })
+    setTodos(updatedTodos)
+}
   return (
     <section>
-        {todos.map(d => <Row key={d.id} todo={d} handleDeleteTodo={handleDeleteTodo}/>)}
+        {todos.map(d => <Row key={d.id} todo={d} handleDeleteTodo={handleDeleteTodo} handleCheckTodo={handleCheckTodo}/>)}
     </section>
   )
 }
